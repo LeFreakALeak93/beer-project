0f74cd7bf65150d85a1e32971cf90339d681ab94
Lukas Jülich <lukas-juelich@web.de>
2023-04-24 17:54:35 +0200

    third commit

---
+++ routes/beer.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = require("express").Router()
+const User = require("../models/User.model")
+const Beer = require("../models/Beer.model")
+
+
+// Get one single beer
+router.get("/beer/create-beer", (req, res) => {
+      res.render("create-beer")
+  });
+
+// Create new beer
+router.post("/beer/create-beer", (req, res) => {
+    Beer.create(req.body)
+      .then((beer) => 
+      res.render("create-beer", { beer: beer }))
+
+      .catch((error) => console.log(error));
+  });
+
+// Go to beer details
+router.get("/beer/beer-details/:beerId", (req, res) => {
+    Beer.findOne({ _id: req.params.beerId })
+      .then((beer) => 
+      res.render("beer-details", { beer: beer }))
+
+      .catch((error) => console.log(error));
+  });
+
+
+// Edit beer details (only admin)
+  router.post("/beer/beer-details/:beerId", (req, res) => {
+    Beer.findOneAndUpdate(
+      { _id: req.params.albumId },
+      req.body,
+    )
+      .then((beer) => 
+      res.render("beer-details", { beer: beer }))
+
+      .catch((error) => console.log(error));
+  });
+
+  // Remove beer (only admin)
+  router.post("/beer/beer-details/:beerId/delete", (req, res) => {
+    Beer.findByIdAndDelete(req.params.albumId)
+
+      .then(() => 
+      res.render("beer-details"))
+
+      .catch((error) => console.log(error));
+  });
+
+
+
+  module.exports = router
