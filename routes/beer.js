d9860d923f26779b6a821ebe2a136ecb084c8a6e
Lukas Jülich <lukas-juelich@web.de>
2023-04-27 15:20:15 +0200

    minor changes

--- routes/beer.js
+++ routes/beer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { uploader, cloudinary } = require("../config/cloudinary.js");
 const router = require("express").Router();
-const User = require("../models/User.model");
+const User = require("../models/User.model.js");
 const Beer = require("../models/Beer.model");
 
 // Go to beer library
